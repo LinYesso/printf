d55c7370121f91208c6e3d1fef788b40d02988dc
Linda Yesso <35770033+LinYesso@users.noreply.github.com>
2022-04-25 16:18:07 +0000

    update print_format.c

--- print_format.c
+++ print_format.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
+#include <unistd.h>   
 #include "main.h"
+                                                                                                                                                                       
+                                                                                                                                                                                                
+/**                                                                                                                                                                                             
+ * _write - writes the character c to stdout                                                                                                                                                    
+ * @c: The character to print                                                                                                                                                                   
+ *                                                                                                                                                                                              
+ * Return: On success 1.                                                                                                                                                                        
+ * On error, -1 is returned, and errno is set appropriately.                                                                                                                                    
+ */                                                                                                                                                                                             
+int _write(char c)                                                                                                                                                                              
+{                                                                                                                                                                                               
+        return (write(1, &c, 1)); 
+}
+
 
 /**
  * _print_format - A function that prints a format
